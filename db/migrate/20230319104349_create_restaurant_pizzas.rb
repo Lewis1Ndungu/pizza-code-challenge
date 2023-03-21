b0281c6edfac007f98c3a0ddfd643381dfc8b673
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2023-03-21 07:39:35 +0300

    Done with challenge

--- db/migrate/20230319104349_create_restaurant_pizzas.rb
+++
@@ -1,9 +0,0 @@
-class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :restaurant_pizzas do |t|
-      t.integer :price
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_104349) do
+ActiveRecord::Schema.define(version: 2023_03_20_170938) do
+
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "restaurant_pizzas", force: :cascade do |t|
     t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "pizza_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pizza_id"], name: "index_restaurant_pizzas_on_pizza_id"
+    t.index ["restaurant_id"], name: "index_restaurant_pizzas_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
